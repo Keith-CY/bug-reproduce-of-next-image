0e454f30f882f78a6d5e96a0cede3a625d2d59c7
Keith <keithwhisper@gmail.com>
2023-10-16 18:49:56 +0900

    chore: code to reproduce a bug of next/image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,12 @@
 /** Add your relevant code here for the issue to reproduce */
+import Image from 'next/image'
+import ClothImg from './5.png'
+
 export default function Home() {
-  return null
+  return (
+    <main>
+      <img src="/images/5.png" width="200" />
+      <Image src={ClothImg} width="200" alt="cloth" />
+    </main>
+  )
 }
